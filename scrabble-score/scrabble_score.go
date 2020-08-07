7f803114371b3a6c9f9ebe5a7fe3b26284e7aa6d
Diego Maia <diegocmsantos@gmail.com>
2020-08-07 12:41:43 +0100

    Scrabble final version

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,27 +1,41 @@
 package scrabble
 
-import (
-	"strings"
-)
+import "unicode"
 
-var pointMap = map[string]int{
-	"AEIOULNRST": 1,
-	"DG":         2,
-	"BCMP":       3,
-	"FHVWY":      4,
-	"K":          5,
-	"JX":         8,
-	"QZ":         10,
+var pointMap = map[rune]int{
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2,
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
 }
 
 // Score receives a word, find the points to each letter and return the sum of the points
 func Score(word string) int {
 	var total int
 	for _, letter := range word {
-		for k, v := range pointMap {
-			total += strings.Count(k, strings.ToUpper(string(letter))) * v
-		}
+		total += pointMap[unicode.ToUpper(letter)]
 	}
-
 	return total
 }
