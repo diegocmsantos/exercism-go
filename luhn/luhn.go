2d2da01119ea7ba1d3d6e146cf0bd18e48d0e804
Diego Maia <diegocmsantos@gmail.com>
2020-10-28 09:23:24 +0000

    luhn and roman numerals

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -7,28 +7,26 @@ import (
 
 // Valid checks if the given input is a valid number based on the Luhn formula
 func Valid(input string) bool {
-
 	input = strings.ReplaceAll(input, " ", "")
 	inputSize := len(input)
-
 	if inputSize <= 1 {
 		return false
 	}
-
-	var number, sum, count int
+	var number, sum int
+	double := false
 	for i := inputSize - 1; i >= 0; i-- {
 		if !unicode.IsDigit(rune(input[i])) {
 			return false
 		}
 		number = int(input[i] - '0')
-		if count%2 != 0 {
+		if double {
 			number *= 2
 			if number > 9 {
 				number -= 9
 			}
 		}
-		sum = sum + number
-		count++
+		sum += number
+		double = !double
 	}
 	return sum%10 == 0
 }
